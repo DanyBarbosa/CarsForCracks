cd551939d6777347c7874a3241173ac2263045e4
AxelDMH <axeldmh.270903@gmail.com>
2024-05-04 17:18:39 -0600

    Acomodo de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AutomovilComponent } from './automovil/automovil.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"home", component:HomeComponent},
+    {path:"autos", component:AutomovilComponent},
+    {path: "contacto" , component: ContactoComponent},
+    {path: "**", pathMatch:"full" ,redirectTo:"home"},
+];
