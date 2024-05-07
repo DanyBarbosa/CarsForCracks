02348c7a53a8cc2780a2f943b2ad8acf5a6007ac
DanyBarbosa <barayadan@hotmail.com>
2024-05-07 10:25:34 -0600

    Prueba 4

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -69,13 +69,6 @@ export class RegistroComponent {
           this.datosCliente.fechaInicio = this.startDate;
           this.datosCliente.fechaFin = this.endDate;
           this.citasService.agregarRenta(this.datosCliente);
-
-          this.datosCliente.nombre = '';
-          this.datosCliente.tel = '';
-          this.datosCliente.correo = '';
-          this.startDate = null;
-          this.endDate = null;
-          this.enviado = false;
         }
       }
     }
